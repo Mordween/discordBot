d85f597f9484b6744f3eadde6b08d58cd19fa6ef
Mordween <al.ridolfi@laposte.net>
2024-09-10 02:04:39 +0200

    BOT DONE

--- commands/utility/stele.js
+++ commands/utility/stele.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, AttachmentBuilder, Attachment } = require('discord.js');
 
 const fs = require('fs');
 
@@ -58,9 +58,47 @@ module.exports = {
         const sublimation = findSublimation(sublimationName);
         const donjon = findDonjon(sublimation.donjon);
 
+        // const file = new AttachmentBuilder('../picture/stele1.jpg');
+        const exampleEmbed = new EmbedBuilder()
+          .setColor(0x0099FF)
+          .setTitle(`${sublimation.name}`)
+          .setDescription(`La sublimation est obtenable dans le dj : ${donjon.name}`)
+          // .setImage('attachment://stele1.jpg')
+
+          .addFields({ name: 'Stele 1', value: `${donjon.stele1}`, inline: false })
+          .addFields({ name: 'Stele 2', value: `${donjon.stele2}`, inline: false })
+          .addFields({ name: 'Stele 3', value: `${donjon.stele3}`, inline: false })
+
+          // .setImage('https://i.imgur.com/AfFp7pu.png')
+          .setTimestamp()
         if (sublimation) {
-            await interaction.reply({ content: `Le donjon associé à **${sublimation.name}** est **${sublimation.donjon}**.`, ephemeral: false });
-            await interaction.followUp({ content: `Les stèles associé à **${donjon.name}** sont **${donjon.stele1}** \n **${donjon.stele2}**.`, ephemeral: false });
+            // await interaction.reply({ content: `Le donjon associé à **${sublimation.name}** est **${sublimation.donjon}**.`, ephemeral: false });
+            // await interaction.followUp({ content: `Les stèles associé à **${donjon.name}** sont :`, ephemeral: false }); 
+            // await interaction.followUp({ content: `**${donjon.stele1}**`, ephemeral: false });
+            // await interaction.followUp({ content: `**${donjon.stele2}**`, ephemeral: false });
+            // await interaction.followUp({ content: `**${donjon.stele3}**`, ephemeral: false });
+            await interaction.reply({ embeds: [exampleEmbed] })
+            if(donjon.name=="Donjon Mansots")
+            {
+              new Promise(resolve => setTimeout(resolve, 10000000))
+              const exampleEmbed = new EmbedBuilder()
+                .setColor(0x0099FF)
+                .setTitle(`${sublimation.name}`)
+                .setDescription(`La sublimation est obtenable dans le dj : ${donjon.name}`)
+                // .setImage('attachment://stele1.jpg')
+
+                .addFields({ name: 'Stele 1', value: `${donjon.stele1T}`, inline: false })
+                .addFields({ name: 'Stele 2', value: `${donjon.stele2T}`, inline: false })
+                .addFields({ name: 'Stele 3', value: `${donjon.stele3T}`, inline: false })
+
+                // .setImage('https://i.imgur.com/AfFp7pu.png')
+                .setTimestamp()
+                setTimeout(function(){ 
+                  interaction.followUp({ content: 'Je rigole, tiens tes stèles', ephemeral: false });
+                  interaction.followUp({ embeds: [exampleEmbed] })
+              }, 10000);
+              
+            }
         } else {
             await interaction.reply({ content: 'Sublimation non trouvée.', ephemeral: true });
         }
